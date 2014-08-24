eb02bba82c9fbfab35114796cd487e905cd2a5ad
Michael Chary <mac389@gmail.com>
2014-08-24 16:10:27 -0400

    updated get tweets from user names

--- src/grab-tweets-from-user.py
+++ src/grab-tweets-from-user.py
@@ -4,22 +4,19 @@ import numpy as np
 from textwrap import TextWrapper
 from string import replace
 
-consumer_key = '0gU0QlrtKLlcidfyfVdH7R1qz'
-consumer_secret = '2GQ8jCjP58xu7gENWwmCTy4vVLRFsvzM2VJL1u7fHcumMqP4qA'
-access_token = '2585785981kfCNAtFtgESI8sT3jw0AhH7qQ6UCdgk4HsWp2If'
-access_token_secret = 'PdATAHsTcnjX9gCgoCVMFcMuffTf3648JHHWC8vhSRQ7z'
+MAX_COUNT = 200
+WRITE ='wb'
+READ = 'rb'
 
+passwords = json.load(open('passwords',READ))
 auth = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-MAX_COUNT = 200
-WRITE ='wb'
-READ = 'rb'
-
-user_names = open('../data/usernames',READ).read().splitlines()
-
+#user_names = open('../data/usernames',READ).read().splitlines()
+NAME = "Person whose age we surmised"
+user_names = ['@'+record[NAME] for record in json.load(open('tweets_with_ages.json',READ))]
 for user_name in user_names:
 	tweets = api.user_timeline(screen_name = user_name, include_rts=False,count=MAX_COUNT)
 	filename = '%stweets'%(replace(user_name,'@',''))
