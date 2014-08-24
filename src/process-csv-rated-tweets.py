df8954c3b34a5a6ae1d33173b0531ac2d6270c5f
Michael Chary <mac389@gmail.com>
2014-08-24 15:44:52 -0400

    update

--- src/process-csv-rated-tweets.py
+++ src/process-csv-rated-tweets.py
@@ -1,8 +1,18 @@
-import csv,os
+import csv,os, json, itertools
+
+from pprint import pprint
 
 base = '/Volumes/My Book/Dropbox/ToxTweet/datasets/age_tweets'
 READ = 'rU'
-
+WRITE = 'wb'
 filenames = [os.path.join(base,filename) for filename in os.listdir(base)]
-data = [record for record in csv.DictReader(open(filename,READ)) for filename in filenames if record['Age'] != '']
 
+verboten = ['1','-1','']
+data = list(itertools.chain(*[[record for record in csv.DictReader(open(filename,READ)) if record['Age'] not in verboten] for filename in filenames]))
+
+savename = 'tweets_with_ages.json'
+full_savename = os.path.join('../data',savename)
+
+
+if not os.path.isfile(full_savename):
+	json.dump(data,open(full_savename,WRITE))
