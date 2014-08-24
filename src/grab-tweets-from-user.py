7bca943bbea16bcc557b956994313c0b6300fee6
Michael Chary <mac389@gmail.com>
2014-08-24 17:45:18 -0400

    update pipe

--- src/grab-tweets-from-user.py
+++ src/grab-tweets-from-user.py
@@ -1,4 +1,4 @@
-import tweepy
+import tweepy,json
 import numpy as np
 
 from textwrap import TextWrapper
@@ -8,20 +8,31 @@ MAX_COUNT = 200
 WRITE ='wb'
 READ = 'rb'
 
-passwords = json.load(open('passwords',READ))
-auth = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+passwords = json.load(open('passwords.json',READ))
+auth = tweepy.auth.OAuthHandler(passwords['consumer_key'], passwords['consumer_secret'])
+auth.set_access_token(passwords['access_token'], passwords['access_token_secret'])
 
 api = tweepy.API(auth)
 
 #user_names = open('../data/usernames',READ).read().splitlines()
 NAME = "Person whose age we surmised"
-user_names = ['@'+record[NAME] for record in json.load(open('tweets_with_ages.json',READ))]
+user_names = ['@'+record[NAME] for record in json.load(open('../data/tweets_with_ages.json',READ))]
+deleted_users = []
 for user_name in user_names:
-	tweets = api.user_timeline(screen_name = user_name, include_rts=False,count=MAX_COUNT)
-	filename = '%stweets'%(replace(user_name,'@',''))
+	try:
+		tweets = api.user_timeline(screen_name = user_name, include_rts=False,count=MAX_COUNT)
+		filename = '../data/%stweets'%(replace(user_name,'@',''))
 
-	#Will get most recent 200 tweets, which samples unevenly in time
-	with open(filename,WRITE) as f:
-		for tweet in tweets:
-			print>>f, tweet.text.encode('ascii','ignore')
+		#Will get most recent 200 tweets, which samples unevenly in time
+		with open(filename,WRITE) as f:
+			for tweet in tweets:
+				print>>f, tweet.text.encode('ascii','ignore')
+	except tweepy.error.TweepError:
+		print 'User %s does not exist anymore'%user_name
+		deleted_users.append(user_name)
+
+with open('../data/deleted_users','a') as f:
+	for deleted_user in deleted_users:
+		print>>f,deleted_user
+
+print '%.02f of the users still existed'%(1-len(deleted_users)/float(len(user_names)))
